8b4154635ec9953e8c6994bbe1ea693d8cf4a385
jngs-park <dear.jayp@gmail.com>
2022-09-14 17:57:06 +0900

    라우팅기능 구현

--- src/main/java/com/ll/exam/DispatchSevelet.java
+++ src/main/java/com/ll/exam/DispatchSevelet.java
@@ -1,10 +1,12 @@
 package com.ll.exam;
 
+import com.ll.exam.article.ArticleController;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import com.ll.exam.member.MemberController;
 
 import java.io.IOException;
 
@@ -12,6 +14,23 @@ import java.io.IOException;
 public class DispatchSevelet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().append("되나?");
+        Rq rq = new Rq(req, resp);
+
+        MemberController memberController = new MemberController();
+        ArticleController articleController = new ArticleController();
+
+        // getRequestURI는
+        // http://localhost:8081/usr/article/list/free?page=1 에서
+        // /usr/article/list/free 부분만 가져온다.
+        String url = req.getRequestURI();
+
+        switch (url) {
+            case "/usr/article/list/free":
+                articleController.showList(rq);
+                break;
+            case "/usr/member/login":
+                memberController.showLogin(rq);
+                break;
+        }
     }
 }
