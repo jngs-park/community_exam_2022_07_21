e7e7b79458dd9f04c7ae074217dba295d748c66c
jngs-park <dear.jayp@gmail.com>
2022-09-15 14:59:03 +0900

    rq.appendBody를 rq.println로 메서드명 변경

--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -42,13 +42,17 @@ public class Rq {
             return defaultValue;
         }
     }
-    public void appendBody(String str) {
+    public void print(String str) {
         try {
             resp.getWriter().append(str);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+    public void println(String str) {
+        print(str + "\n");
+    }
+
     public void setAttr(String name, Object value) {
         req.setAttribute(name, value);
     }
@@ -81,14 +85,14 @@ public class Rq {
     public long getLongPathValueByIndex(int index, long defaultValue) {
         String value = getPathValueByIndex(index, null);
 
-        if ( value == null ) {
+        if (value == null) {
             return defaultValue;
         }
 
         try {
             return Long.parseLong(value);
         }
-        catch ( NumberFormatException e ) {
+        catch (NumberFormatException e) {
             return defaultValue;
         }
     }
--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -30,21 +30,21 @@ public class ArticleController {
 
         long id = articleService.write(title, body);
 
-        rq.appendBody("%d번 게시물이 생성 되었습니다.".formatted(id));
+        rq.println("%d번 게시물이 생성 되었습니다.".formatted(id));
     }
 
     public void showDetail(Rq rq) {
         long id = rq.getLongPathValueByIndex(1, 0);
 
         if (id == 0) {
-            rq.appendBody("번호를 입력해주세요.");
+            rq.println("번호를 입력해주세요.");
             return;
         }
 
         ArticleDto articleDto = articleService.findById(id);
 
         if (articleDto == null) {
-            rq.appendBody("해당 글이 존재하지 않습니다.");
+            rq.println("해당 글이 존재하지 않습니다.");
             return;
         }
 
@@ -56,35 +56,35 @@ public class ArticleController {
         long id = rq.getLongPathValueByIndex(1, 0);
 
         if (id == 0) {
-            rq.appendBody("번호를 입력해주세요.");
+            rq.println("번호를 입력해주세요.");
             return;
         }
 
         ArticleDto articleDto = articleService.findById(id);
 
         if (articleDto == null) {
-            rq.appendBody("해당 글이 존재하지 않습니다.");
+            rq.println("해당 글이 존재하지 않습니다.");
             return;
         }
 
         articleService.delete(id);
 
-        rq.appendBody("<div>%d번 게시물이 삭제되었습니다.</div>".formatted(id));
-        rq.appendBody("<div><a href=\"/usr/article/list/free\">리스트로 이동</a></div>".formatted(id));
+        rq.println("<div>%d번 게시물이 삭제되었습니다.</div>".formatted(id));
+        rq.println("<div><a href=\"/usr/article/list/free\">리스트로 이동</a></div>");
     }
 
     public void showModify(Rq rq) {
         long id = rq.getLongPathValueByIndex(1, 0);
 
         if (id == 0) {
-            rq.appendBody("번호를 입력해주세요.");
+            rq.println("번호를 입력해주세요.");
             return;
         }
 
         ArticleDto articleDto = articleService.findById(id);
 
         if (articleDto == null) {
-            rq.appendBody("해당 글이 존재하지 않습니다.");
+            rq.println("해당 글이 존재하지 않습니다.");
             return;
         }
 
@@ -97,8 +97,8 @@ public class ArticleController {
         String title = rq.getParam("title", "");
         String body = rq.getParam("body", "");
 
-        rq.appendBody("<div>id : %d</div>".formatted(id));
-        rq.appendBody("<div>title : %s</div>".formatted(title));
-        rq.appendBody("<div>body : %s</div>".formatted(body));
+        rq.println("<div>%d번 게시물이 수정되었습니다.</div>".formatted(id));
+        rq.println("<div><a href=\"/usr/article/detail/free/%d\">수정된 글로 이동</a></div>".formatted(id));
+
     }
 }
